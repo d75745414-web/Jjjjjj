231698f2c0f50ca3aad8c6a4d793b853b20198db
c00lkiddiskingnow <d75745414@gmail.com>
2026-03-18 00:30:32 +0000

    Simplify games handler response
    Refactor handler to return an empty array if no games are found.

--- api/games.js
+++ api/games.js
@@ -1,16 +1,3 @@
-import { kv } from "@vercel/kv";
-
-export default async function handler(req, res) {
-    const ids = await kv.smembers("games:list");
-
-    const games = [];
-
-    for (const id of ids) {
-        const game = await kv.get("game:" + id);
-        if (game) {
-            games.push(game);
-        }
-    }
-
-    res.status(200).json(games);
+export default function handler(req, res) {
+    res.status(200).json(games || []);
 }
